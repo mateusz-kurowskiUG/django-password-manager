876c1f59c62b2149ed4cbe0b5705df319f2288a3
Mateusz Kurowski <82779261+MatKur1331@users.noreply.github.com>
2023-04-03 19:17:16 +0200

    y

---
+++ Beyond_the_basic_stuff_with_python/kwargss.py
@@ -0,0 +1,14 @@
+def funk(liczba, ile_razy):
+    print(liczba * ile_razy)
+
+
+slownik = {"liczba": 5, "ile_razy": 2}
+
+funk(**slownik)
+
+
+def int_to_string(num: int) -> str:
+    return str(num)
+
+
+git
