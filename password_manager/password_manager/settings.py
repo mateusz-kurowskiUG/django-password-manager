c7c46b61d59fb7815ec3b7a710d0b62289e21bae
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2023-04-30 00:39:25 +0200

    database migrated to postgres.

--- password_manager/password_manager/settings.py
+++ password_manager/password_manager/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'password_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME":'passwd_manager_db',
+        "USER":"postgres",
+        "PASSWORD":"root",
+        "HOST":"localhost",
+        "PORT":"5432"
     }
 }
 
